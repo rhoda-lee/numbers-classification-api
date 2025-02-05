67c350f491929314c1fe14b5b5e89be5e52ff7a6
rhoda-lee <rhodalee.dev@gmail.com>
2025-02-05 22:28:36 +0000

    added cors handling

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, Query, HTTPException
 import requests
 from utils import NumberUtils
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 NUMBERS_API_URL = "http://numbersapi.com"
 
